629fbc78476663c113f8714856fdd5e58adc5d9a
mehdiboutoumit <mehdiboutoumit03@gmail.com>
2023-10-01 22:50:20 +0000

    Added some skills

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -1,4 +1,13 @@
 // Import images
+import reacticon from "../images/skills/react.png";
+import nodeicon from "../images/skills/node.png";
+import express from "../images/skills/express.png";
+import laravel from "../images/skills/laravel.png";
+import spring from "../images/skills/spring-boot.png";
+import js from "../images/skills/js.png";
+import mysql from "../images/skills/mysql.png";
+import rest  from "../images/skills/react.png";
+
 
 
 export const skillsHeading = 'Skills';
@@ -7,6 +16,35 @@ export const skillsData = [
 	{
 		id: 1,
 		title: 'React',
-		img: "",
-	}
+		img: reacticon,
+	},{
+		id: 2,
+		title: 'Node',
+		img: nodeicon,
+	},
+	,{
+		id: 2,
+		title: 'Node',
+		img: laravel,
+	},,{
+		id: 2,
+		title: 'Node',
+		img: spring,
+	},,{
+		id: 2,
+		title: 'Node',
+		img: express,
+	},,{
+		id: 2,
+		title: 'Node',
+		img: rest,
+	},,{
+		id: 2,
+		title: 'Node',
+		img: mysql,
+	},,{
+		id: 2,
+		title: 'Node',
+		img: js,
+	},
 ];
