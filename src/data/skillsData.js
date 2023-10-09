bd070f260f581f6a05b152b5422dcd4a11eb8e00
mehdiboutoumit <mehdiboutoumit03@gmail.com>
2023-10-09 22:53:01 +0000

    update skills

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -6,7 +6,11 @@ import laravel from "../images/skills/laravel.png";
 import spring from "../images/skills/spring-boot.png";
 import js from "../images/skills/js.png";
 import mysql from "../images/skills/mysql.png";
-import rest  from "../images/skills/react.png";
+import rest  from "../images/skills/rest.svg";
+import boostrap from "../images/skills/bootstrap.png";
+import mui from "../images/skills/materialui.png";
+import docker from "../images/skills/docker.png";
+import kuber from "../images/skills/kubernetes.png";
 
 
 
@@ -23,28 +27,44 @@ export const skillsData = [
 		img: nodeicon,
 	},
 	,{
-		id: 2,
-		title: 'Node',
+		id: 3,
+		title: 'laravel',
 		img: laravel,
 	},,{
-		id: 2,
-		title: 'Node',
+		id: 4,
+		title: 'spring',
 		img: spring,
 	},,{
-		id: 2,
-		title: 'Node',
+		id: 5,
+		title: 'express',
 		img: express,
 	},,{
-		id: 2,
-		title: 'Node',
+		id: 6,
+		title: 'rest',
 		img: rest,
 	},,{
-		id: 2,
-		title: 'Node',
+		id: 7,
+		title: 'mysql',
 		img: mysql,
 	},,{
-		id: 2,
-		title: 'Node',
+		id: 8,
+		title: 'js',
 		img: js,
+	},,{
+		id: 9,
+		title: 'boot',
+		img: boostrap,
+	},,{
+		id: 10,
+		title: 'mui',
+		img: mui,
+	},,{
+		id: 11,
+		title: 'docker',
+		img: docker,
+	},,{
+		id: 12,
+		title: 'kuber',
+		img: kuber,
 	},
 ];
