02fb75e1be8950076cc40e8b8ff83ff3883bdfe5
mehdiboutoumit <mehdiboutoumit03@gmail.com>
2023-11-14 15:16:43 +0000

    change profile picture

--- src/hooks/useThemeSwitcher.jsx
+++ src/hooks/useThemeSwitcher.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-
 const useThemeSwitcher = () => {
 	const [theme, setTheme] = useState(localStorage.theme);
 	const activeTheme = theme === 'dark' ? 'light' : 'dark';
